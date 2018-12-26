1d3d9fc852b4c6838796c5d504d3e08a9a129d9e
Esdras R. Carmo <esdras.carmo@agronow.com.br>
2018-12-26 16:19:59 -0200

    generating array X and y

--- gen_dataset.py
+++ gen_dataset.py
@@ -7,9 +7,13 @@ import collections
 import config
 import re
 import pickle
+import numpy as np
+
+def split_text(text):
+    return re.findall(r"[\w']+|[.,!?;]", text)
 
 def generate_dict(text):
-    words = re.findall(r"[\w']+|[.,!?;]", text)
+    words = split_text(text)
 
     count = collections.Counter(words).most_common()
     word_indices = dict()
@@ -25,6 +29,23 @@ def generate_dict(text):
 
     return word_indices, indices_word, ignored_words
 
+def gen_sequences(lines, wi, iw, ign, sequence_len, step=1):
+    sequences = []
+    next_word = []
+    for line in lines:
+        words = split_text(line)
+
+        # checks if line doesn't have a ignored word
+        # create sequence only if number of words is greater than sequence_Len
+        if len(set(words).intersection(ign)) == 0 and len(words) > sequence_len:
+            for i in range(0, len(words) - sequence_len, step):
+                seq = list(map(lambda w: wi[w], words[i:i + sequence_len]))
+
+                sequences.append(np.array(seq))
+                next_word.append(wi[words[i+sequence_len]])
+
+    return np.array(sequences), np.array(next_word)
+
 def main():
     text = letrasmus.scrap(config.OUTPUT_DATASET_FILE + '.txt', n_songs=config.NUM_SONGS)
 
@@ -34,6 +55,15 @@ def main():
     pickle.dump(indices_word, open(config.OUTPUT_DATASET_FILE + '.iw.pkl', 'wb'))
     pickle.dump(ignored_words, open(config.OUTPUT_DATASET_FILE + '.ign.pkl', 'wb'))
     print('done!')
+
+    print('generating sequences...')
+    sequences, next_word = gen_sequences(text.split('\n'), word_indices, indices_word, ignored_words, config.SEQUENCE_LEN)
+    print('sequences shape: ', sequences.shape)
+
+    np.save(config.OUTPUT_DATASET_FILE + '.x.npy', sequences)
+    np.save(config.OUTPUT_DATASET_FILE + '.y.npy', next_word)
+    print('done!')
+
 
 if __name__ == '__main__':
     main()
